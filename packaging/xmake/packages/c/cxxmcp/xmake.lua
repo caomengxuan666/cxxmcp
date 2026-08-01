cc81e3a7f3ae4a9f1f2c98f944c3492fe6f4046c
caomengxuan666 <2507560089@qq.com>
2026-08-02 00:01:07 +0800

    chore: prepare v1.2.5 release

--- packaging/xmake/packages/c/cxxmcp/xmake.lua
+++ packaging/xmake/packages/c/cxxmcp/xmake.lua
@@ -4,7 +4,7 @@ package("cxxmcp")
     set_license("MIT")
 
     add_urls("https://github.com/caomengxuan666/cxxmcp/releases/download/$(version)/cxxmcp-sdk-source-$(version).tar.gz")
-    add_versions("v1.2.4", "984258b9283316e15707b69b48b8d22188bf237a5e4d67929e54812da3eb5d2c")
+    add_versions("v1.2.5", "5772c5ef9ae8b465d9f8def655815cb17a7eb3d7fa8aa2477844316ccf7d5a5d")
     add_configs("http", {description = "Build HTTP/SSE transport (requires cpp-httplib).", default = false, type = "boolean"})
     add_configs("websocket", {description = "Build WebSocket transport (implies HTTP/cpp-httplib support).", default = false, type = "boolean"})
     add_configs("auth", {description = "Build the optional OAuth 2.1 / DPoP auth scaffold target.", default = false, type = "boolean"})
